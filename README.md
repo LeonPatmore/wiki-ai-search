6e2e07a93e98f4244f609456d6fa77bbc1e128fd
Leon Patmore <leon.patmore@bath.edu>
2023-08-02 00:20:32 +0100

    improvements and readme.

---
+++ README.md
@@ -0,0 +1,5 @@
+# AI Wiki Search
+
+A generic application for searching documents.
+
+Uses OpenAI API, using GPT4 model.
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import gradio as gr
-from langchain import OpenAI
+from gradio import Textbox, Interface
+from langchain.chat_models import ChatOpenAI
 from llama_index import ServiceContext, GPTVectorStoreIndex, LLMPredictor, PromptHelper, SimpleDirectoryReader
 from llama_index import StorageContext, load_index_from_storage
-from llama_index.node_parser import SimpleNodeParser
-
-parser = SimpleNodeParser()
 
 
 def construct_index(directory_path):
@@ -13,43 +10,38 @@ def construct_index(directory_path):
     chunk_size_limit = 1024
 
     print("*" * 5, "Documents parsing initiated", "*" * 5)
-    file_metadata = lambda x: {"filename": x}
-    reader = SimpleDirectoryReader(directory_path, file_metadata=file_metadata)
+    reader = SimpleDirectoryReader(directory_path)
     documents = reader.load_data()
 
     # nodes = parser.get_nodes_from_documents(documents)
     # index = GPTVectorStoreIndex(nodes)
     prompt_helper = PromptHelper(max_input_size, num_outputs, chunk_size_limit=chunk_size_limit)
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="gpt-4", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=1, model_name="gpt-4", max_tokens=num_outputs))
 
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
 
-    # print("*"*5, "Index creation initiated", "*"*5)
-    index = GPTVectorStoreIndex.from_documents(
-        documents=documents, service_context=service_context
-    )
-    # print("*"*5, "Index created", "*"*5)
+    index = GPTVectorStoreIndex.from_documents(documents=documents, service_context=service_context)
     index.storage_context.persist("./entire_docs")
-    return index
 
+
+def load_query_engine():
+    storage_context = StorageContext.from_defaults(persist_dir="./entire_docs")
+    loaded_index = load_index_from_storage(storage_context)
+    return loaded_index.as_query_engine()
+
+
+def ask_bot(text_input: str):
+    return query_engine.query(text_input)
 
 
 construct_index("./docs")
-storage_context = StorageContext.from_defaults(persist_dir="./entire_docs")
-index = load_index_from_storage(storage_context)
-query_engine = index.as_query_engine()
+query_engine = load_query_engine()
 
+iface = Interface(fn=ask_bot,
+                  inputs=Textbox(lines=7, label="Enter your text"),
+                  outputs="text",
+                  title="Wiki Search")
 
-def ask_bot(input: str) -> str:
-    return query_engine.query(input)
-
-
-iface = gr.Interface(fn=ask_bot,
-                     inputs=gr.inputs.Textbox(lines=7, label="Enter your text"),
-                     outputs="text",
-                     title="Custom-trained AI Chatbot")
-
-index = construct_index("docs")
 iface.launch(share=False, server_name="0.0.0.0")
 
 if __name__ == '__main__':
